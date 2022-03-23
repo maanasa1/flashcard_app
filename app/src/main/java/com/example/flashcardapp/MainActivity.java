55bc80a22a3e006f88556cc885386d43ee7a38bc
maanasa1 <maanasap@usf.edu>
2022-03-23 15:59:43 -0400

    Lab 3

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -9,57 +9,87 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView flashcard_question;
-    TextView flashcard_answer;
+    TextView flashcardQuestion;
+    TextView flashcardAnswer;
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int cardIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        flashcard_question = findViewById(R.id.flashcard_question_textview);
-        flashcard_answer = findViewById(R.id.flashcard_answer_textview);
 
-        flashcard_answer.setVisibility(View.INVISIBLE);
+        flashcardQuestion = findViewById(R.id.flashcardQuestionTextview);
+        flashcardAnswer = findViewById(R.id.flashcardAnswerTextview);
+        flashcardAnswer.setVisibility(View.INVISIBLE);
+        findViewById(R.id.edit_flashcard_button).setVisibility(View.INVISIBLE);
 
-        flashcard_question.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                flashcard_answer.setVisibility(View.VISIBLE);
-                flashcard_question.setVisibility(View.INVISIBLE);
-            }
+        flashcardQuestion.setOnClickListener(view -> {
+            flashcardAnswer.setVisibility(View.VISIBLE);
+            flashcardQuestion.setVisibility(View.INVISIBLE);
         });
 
-        flashcard_answer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                flashcard_answer.setVisibility(View.INVISIBLE);
-                flashcard_question.setVisibility(View.VISIBLE);
-            }
+        flashcardAnswer.setOnClickListener(view -> {
+            flashcardAnswer.setVisibility(View.INVISIBLE);
+            flashcardQuestion.setVisibility(View.VISIBLE);
         });
 
-        ImageView add_flashcard = findViewById(R.id.add_flashcard_button);
-        add_flashcard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                //startActivity(intent);
-                startActivityForResult(intent, 100);
+        ImageView addFlashcard = findViewById(R.id.add_flashcard_button);
+        addFlashcard.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+            startActivityForResult(intent, 100);
+        });
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        if(allFlashcards != null && allFlashcards.size() > 0) {
+            Flashcard firstCard = allFlashcards.get(0);
+            flashcardQuestion.setText(firstCard.getQuestion());
+            flashcardAnswer.setText(firstCard.getAnswer());
+        }
+
+        findViewById(R.id.next_flashcard_button).setOnClickListener(view -> {
+            if(allFlashcards == null || allFlashcards.size() == 0) {
+                return;
             }
+            cardIndex++;
+
+            if(cardIndex >= allFlashcards.size()){
+                Snackbar.make(view, "You've reached the end of the cards! Going back to the start",
+                        Snackbar.LENGTH_SHORT).show();
+                cardIndex = 0; //reset index so that user can go back to the beginning of the cards
+            }
+
+            Flashcard currentCard = allFlashcards.get(cardIndex);
+            flashcardQuestion.setText(currentCard.getQuestion());
+            flashcardAnswer.setText(currentCard.getAnswer());
         });
     }
 
         @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data) {
             super.onActivityResult(requestCode, resultCode, data);
-            if(requestCode == 100){
-                if(data != null) { //null check
-                    String question_string = data.getExtras().getString("QUESTION KEY");
-                    String answer_string = data.getExtras().getString("ANSWER KEY");
-                    flashcard_question.setText(question_string);
-                    flashcard_answer.setText(answer_string);
+            if (requestCode == 100) {
+                if (data != null) { //null check
+                    String questionString = data.getExtras().getString("QUESTION KEY");
+                    String answerString = data.getExtras().getString("ANSWER KEY");
+                    flashcardQuestion.setText(questionString);
+                    flashcardAnswer.setText(answerString);
+
+                    Flashcard flashcard = (new Flashcard(questionString, answerString));
+                    flashcardDatabase.insertCard(flashcard);
+                    allFlashcards = flashcardDatabase.getAllCards();
+
                 }
+            }
         }
-    }
 }
